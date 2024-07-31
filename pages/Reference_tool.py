5f41d6d0a123434bfe5bed624048dd47dd70e423
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-31 17:12:21 +0100

    sd

--- pages/Reference_tool.py
+++ pages/Reference_tool.py
@@ -658,9 +658,10 @@ else:
                     no_authors = df_authorships['author_name'].nunique()
                     no_doi_found = df_final['DOI'].nunique()
                     no_country = df_authorships['Country Code 3'].nunique()
+                    no_work = df_authorships['OpenAlex_ID']
 
-                    st.info(f'Results found for {no_doi_found} DOIs out of {no_dois}')
-                    col1, col2, col3 = st.columns(3)
+                    st.info(f'{no_work} references found for the DOI {df_authorships['doi']}.')
+                    col1, col2, col3, col4 = st.columns(4)
                     with col1:
                         st.metric(
                             label=f'Citation Source Index', 
@@ -671,6 +672,8 @@ else:
                     with col2:
                         st.metric(label=f'Number of unique authors', value=f'{no_authors}')
                     with col3:
+                        st.metric(label=f'Number of unique author countries', value=f'{no_country}')
+                    with col4:
                         st.metric(label=f'Number of unique author countries', value=f'{no_country}')
                     
                     @st.experimental_fragment
