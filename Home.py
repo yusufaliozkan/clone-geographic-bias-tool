0601af454ffaba2cb197b3c4aeb089617b926381
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-01 10:21:33 +0100

    sa

--- Home.py
+++ Home.py
@@ -11,11 +11,11 @@ import numpy as np
 import plotly.express as px
 import time
 
-# st.set_page_config(layout = "wide", 
-#                     page_title='Geographic Bias Tool',
-#                     page_icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtoX76TyVQs-o1vEvNuAnYX0zahtSui173gg&s",
-#                     initial_sidebar_state="auto") 
-# pd.set_option('display.max_colwidth', None)
+st.set_page_config(layout = "wide", 
+                    page_title='Geographic Bias Tool',
+                    page_icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtoX76TyVQs-o1vEvNuAnYX0zahtSui173gg&s",
+                    initial_sidebar_state="auto") 
+pd.set_option('display.max_colwidth', None)
 
 home_page = st.Page('Home.py', title='Affiliation finder')
 reference_finder = st.Page('Reference_finder.py', title='Reference finder')
