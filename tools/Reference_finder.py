52049fba851260c17bfcb2aad8f19dd09fa5af8a
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-01 15:51:03 +0100

    ds

--- tools/Reference_finder.py
+++ tools/Reference_finder.py
@@ -203,7 +203,7 @@ else:
                 df_exploded = df_dois.explode('referenced_works')
                 df_exploded
 
-                if df_exploded['referenced_works'].isnull().all():
+                if df_exploded['title_of_original_work'].isnull().all():
                     st.error(f'''
                     No reference found for **{df_dois['doi'].iloc[0]}**! 
 
