eb1d5dbe26ff6583e6a1dd302afd88ea32a34ca0
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-01 10:16:59 +0100

    ds

--- Home.py
+++ Home.py
@@ -13,7 +13,7 @@ import time
 
 home_page = st.Page('Home.py', title='Affiliation finder')
 reference_finder = st.Page('Reference_finder.py', title='Reference finder')
-reference_finder2 = st.Page('Reference_finder.py', title='Reference finder2')
+reference_finder2 = st.Page('Reference_finder copy.py', title='Reference finder2')
 
 pg = st.navigation([home_page, reference_finder, reference_finder2])
 
