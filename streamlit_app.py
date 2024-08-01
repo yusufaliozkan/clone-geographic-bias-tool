fd843b89eda8c2d3b31f71ecfcfdae4b70ddc5d9
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-01 12:04:22 +0100

    sd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,27 @@
 import streamlit as st
+import pandas as pd
+import requests
+import xml.etree.ElementTree as ET
+import streamlit.components.v1 as components
+import xml.etree.ElementTree as ET
+import matplotlib.pyplot as plt
+import plotly.express as px
+from copyright import display_custom_license
+import numpy as np
+import plotly.express as px
+import time
 
-home_page = st.Page('Home.py', title='Affiliation finder')
-reference_finder = st.Page('Reference_finder.py', title='Reference finder')
-reference_finder2 = st.Page('Reference_finder copy.py', title='Reference finder2')
+# home_page = st.Page('Home.py', title='Affiliation finder')
 
-pg = st.navigation([home_page, reference_finder, reference_finder2])
-st.set_page_config(page_title="Affiliationfinder", page_icon=":material/edit:")
+affiliation_finder = st.Page('tools/Affiliation_finder.py', title='Publication affiliation finder')
+reference_finder = st.Page('tools/Reference_finder.py', title='Reference affiliation finder')
+intro = st.Page('home/Intro.py', title='Introduction')
 
+pg = st.navigation(
+    {
+        'Home':[intro],
+        'Tools':[affiliation_finder, reference_finder]
+    }
+)
+    
 pg.run()
