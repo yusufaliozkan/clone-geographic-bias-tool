a0990627cde3556b1ac3bff8d570b23aa1dfa6e3
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-01 10:18:42 +0100

    sd

--- Home.py
+++ Home.py
@@ -11,17 +11,17 @@ import numpy as np
 import plotly.express as px
 import time
 
-# st.set_page_config(layout = "wide", 
-#                     page_title='Geographic Bias Tool',
-#                     page_icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtoX76TyVQs-o1vEvNuAnYX0zahtSui173gg&s",
-#                     initial_sidebar_state="auto") 
-# pd.set_option('display.max_colwidth', None)
+st.set_page_config(layout = "wide", 
+                    page_title='Geographic Bias Tool',
+                    page_icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtoX76TyVQs-o1vEvNuAnYX0zahtSui173gg&s",
+                    initial_sidebar_state="auto") 
+pd.set_option('display.max_colwidth', None)
 
-# home_page = st.Page('Home.py', title='Affiliation finder')
+home_page = st.Page('Home.py', title='Affiliation finder')
 reference_finder = st.Page('Reference_finder.py', title='Reference finder')
 reference_finder2 = st.Page('Reference_finder copy.py', title='Reference finder2')
 
-pg = st.navigation([reference_finder, reference_finder2])
+pg = st.navigation([home_page, reference_finder, reference_finder2])
 
 pg.run()
 
