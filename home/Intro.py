84b6045f77cc5aa686db8610677c0d43a882bc75
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-01 11:44:08 +0100

    ds

--- home/Intro.py
+++ home/Intro.py
@@ -56,7 +56,7 @@ st.write('''
 st.subheader('Navigate to the available tools:', anchor=False)
 st.page_link(
     'tools/Affiliation_finder.py', 
-    label=f'_Publication affiliation finder_', 
+    label=f'__Publication affiliation finder__', 
     icon="🔗", 
     help='This tool helps you identify the country affiliations of authors for multiple publications.'
     )
