fbad40b74693135e1d0ffbe08346ac135b6d7a90
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-10-29 10:24:25 +0000

    ds

--- tools/Affiliation_finder.py
+++ tools/Affiliation_finder.py
@@ -498,7 +498,7 @@ else:
                             fig2 = px.pie(income_level_counts, names='Income Level', values='Count', title='Income Level Counts')
                             col2.plotly_chart(fig2, use_container_width = True)
 
-                        col1, col2 = st.columns([3,1])
+                        col1, col2 = st.columns([3,2])
                         with col1:
                             # Function to get coordinates
                             def get_coordinates(country_name):
