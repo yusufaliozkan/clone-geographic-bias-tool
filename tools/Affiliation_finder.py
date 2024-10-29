5cd507159b14453fb3f11b57d8568366e7eda202
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-10-29 10:21:43 +0000

    ds

--- tools/Affiliation_finder.py
+++ tools/Affiliation_finder.py
@@ -498,54 +498,57 @@ else:
                             fig2 = px.pie(income_level_counts, names='Income Level', values='Count', title='Income Level Counts')
                             col2.plotly_chart(fig2, use_container_width = True)
 
-                        # Function to get coordinates
-                        def get_coordinates(country_name):
-                            try:
-                                country = CountryInfo(country_name)
-                                return country.info().get('latlng', (None, None))
-                            except KeyError:
-                                return None, None
+                        col1, col2 = st.columns([3,1])
+                        with col1:
+                            # Function to get coordinates
+                            def get_coordinates(country_name):
+                                try:
+                                    country = CountryInfo(country_name)
+                                    return country.info().get('latlng', (None, None))
+                                except KeyError:
+                                    return None, None
 
-                        # Apply the function to each country to get latitude and longitude
-                        country_counts[['Latitude', 'Longitude']] = country_counts['Country Name'].apply(lambda x: pd.Series(get_coordinates(x)))
+                            # Apply the function to each country to get latitude and longitude
+                            country_counts[['Latitude', 'Longitude']] = country_counts['Country Name'].apply(lambda x: pd.Series(get_coordinates(x)))
 
-                        # Set a scaling factor and minimum radius to make circles larger
-                        scaling_factor = 1000  # Adjust this to control the overall size of the circles
-                        minimum_radius = 100000  # Minimum radius for visibility of all points
+                            # Set a scaling factor and minimum radius to make circles larger
+                            scaling_factor = 1000  # Adjust this to control the overall size of the circles
+                            minimum_radius = 100000  # Minimum radius for visibility of all points
 
-                        # Calculate the circle size based on `Count`
-                        country_counts['size'] = country_counts['Count'] * scaling_factor + minimum_radius
+                            # Calculate the circle size based on `Count`
+                            country_counts['size'] = country_counts['Count'] * scaling_factor + minimum_radius
 
-                        # Filter out rows where coordinates were not found
-                        country_counts = country_counts.dropna(subset=['Latitude', 'Longitude'])
+                            # Filter out rows where coordinates were not found
+                            country_counts = country_counts.dropna(subset=['Latitude', 'Longitude'])
 
-                        # ScatterplotLayer to show countries and their mentions count
-                        scatterplot_layer = pdk.Layer(
-                            "ScatterplotLayer",
-                            data=country_counts,
-                            get_position=["Longitude", "Latitude"],
-                            get_radius="size",
-                            get_fill_color="[255, 140, 0, 160]",  # Adjusted color with opacity
-                            pickable=True,
-                            auto_highlight=True,
-                            id="country-mentions-layer",
-                        )
+                            # ScatterplotLayer to show countries and their mentions count
+                            scatterplot_layer = pdk.Layer(
+                                "ScatterplotLayer",
+                                data=country_counts,
+                                get_position=["Longitude", "Latitude"],
+                                get_radius="size",
+                                get_fill_color="[255, 140, 0, 160]",  # Adjusted color with opacity
+                                pickable=True,
+                                auto_highlight=True,
+                                id="country-mentions-layer",
+                            )
 
-                        # Define the view state of the map
-                        view_state = pdk.ViewState(
-                            latitude=20, longitude=0, zoom=1, pitch=30
-                        )
+                            # Define the view state of the map
+                            view_state = pdk.ViewState(
+                                latitude=20, longitude=0, zoom=1, pitch=30
+                            )
 
-                        # Create the Deck with the layer, view state, and map style
-                        chart = pdk.Deck(
-                            layers=[scatterplot_layer],
-                            initial_view_state=view_state,
-                            tooltip={"text": "{Country Name}\n# Authors: {Count}"},
-                            map_style="mapbox://styles/mapbox/light-v9"  # Use a light map style
-                        )
-                        st.subheader('Author country affiliations', anchor=False)
-                        st.pydeck_chart(chart, use_container_width=False)
-            
+                            # Create the Deck with the layer, view state, and map style
+                            chart = pdk.Deck(
+                                layers=[scatterplot_layer],
+                                initial_view_state=view_state,
+                                tooltip={"text": "{Country Name}\n# Authors: {Count}"},
+                                map_style="mapbox://styles/mapbox/light-v9"  # Use a light map style
+                            )
+                            st.subheader('Author country affiliations', anchor=False)
+                            st.pydeck_chart(chart, use_container_width=False)
+                        with col2:
+                            country_counts
                     gbi_tool()
  
                     @st.experimental_fragment
