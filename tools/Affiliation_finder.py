2653a7b97069f0ba860cd55599edbacc09779cf2
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2026-04-01 10:14:06 +0100

    sd

--- tools/Affiliation_finder.py
+++ tools/Affiliation_finder.py
@@ -494,6 +494,7 @@ else:
                             country_counts = df_authorships['Country Name'].value_counts().reset_index()
                             country_counts.columns = ['Country Name', 'Count']
                             fig = px.bar(country_counts, x='Country Name', y='Count', title='Country Counts')
+                            fig.update_xaxes(tickangle=-45)
                             col1.plotly_chart(fig, use_container_width = True)
                             country_counts = pd.merge(country_counts, df_result, on='Country Name')
                             country_counts = country_counts.drop(columns=['Unnamed: 0', 'Country Code 3', 'Country Code 2', 'name', 'Year','GNI'])
